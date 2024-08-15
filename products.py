a058a2ba5083a366093af16b03e1493dc45cc07c
e-kif <yevhen>
2024-08-16 01:46:13 +0300

    Product methods are added

--- products.py
+++ products.py
@@ -10,6 +10,53 @@ class Product:
         if not isinstance(quantity, int) or quantity < 0:
             raise ValueError('Quantity should be a positive number.')
         self._quantity = quantity
+        self._active = True
+
+    def get_quantity(self):
+        return self._quantity
+
+    def set_quantity(self, quantity):
+        if not isinstance(quantity, int) or quantity < 0:
+            raise ValueError('Quantity should be a positive integer.')
+        self._quantity = quantity
+        if self._quantity == 0:
+            self.deactivate()
+
+    def is_active(self):
+        return self._active
+
+    def activate(self):
+        self._active = True
+
+    def deactivate(self):
+        self._active = False
+
+    def show(self):
+        return f'{self._name}, Price: {self._price}, Quantity: {self._quantity}'
+
+    def buy(self, quantity):
+        if not isinstance(quantity, int) or quantity <= 0:
+            raise ValueError('Quantity should be a positive integer.')
+        if quantity > self._quantity:
+            raise ValueError(f'Quantity can not be bigger than items in store ({self._quantity}).')
+        self.set_quantity(self._quantity - quantity)
+        return quantity * self._price
 
 
-bose = Product('Bose QuietComfort Earbuds', 250, 500)
+def main():
+    bose = Product('Bose QuietComfort Earbuds', 250, 500)
+    mac = Product('MacBook Air M2', 1450, 100)
+
+    print(bose.buy(50))
+    print(mac.buy(100))
+    print(mac.is_active())
+
+    print(bose.show())
+    print(mac.show())
+
+    bose.set_quantity(1000)
+    print(bose.show())
+
+
+if __name__ == "__main__":
+    main()
